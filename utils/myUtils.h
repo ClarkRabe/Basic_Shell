560d28692f09e72c4cfab2b3dc260eabae52ddaa
ClarkRabe <45646499+ClarkRabe@users.noreply.github.com>
2019-12-18 16:14:24 -0800

    Add files via upload

---
+++ utils/myUtils.h
@@ -0,0 +1,18 @@
+
+#ifndef MYUTILS_H
+#define MYUTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 100
+
+
+
+int menu();
+
+void strip(char *array);
+
+char* parse(char* toParse, char* delim);
+#endif
